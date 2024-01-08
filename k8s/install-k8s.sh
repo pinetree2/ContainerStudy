d5130d3e3d994edcbf8993996aa9bdf368c6f8dd
pinetree2 <pinetree3966@naver.com>
2024-01-08 17:33:34 +0900

    k8s yaml added

---
+++ k8s/install-k8s.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
+
+apt-get update
+apt-get install -y kubectl
+
+git clone https://github.com/ahmetb/kubectx /opt/kubectx
+ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+ln -s /opt/kubectx/kubens /usr/local/bin/kubens
